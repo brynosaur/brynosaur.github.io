40e78a57a7369ec71c6e689461bd267e56e8c827
Bryan Kveton <bkveton2@gmail.com>
2015-07-15 16:26:06 -0500

    Add contact form

--- js/main.js
+++ js/main.js
@@ -23,4 +23,54 @@
     }, 100));
   });
 
+  /* Contact Form */
+
+  var $contactForm = $('#contact-form');
+
+  $contactForm.submit(function(e) {
+
+    e.preventDefault();
+
+    var errorcount = 0;
+
+    $('input[type="text"], input[type="email"]').each(function() {
+        iValue = $(this).val();
+        if(iValue == '') {
+            $(this).addClass('error');
+            errorcount++;
+        }
+    });
+
+    if(errorcount == 0) {
+
+      $.ajax({
+        url: '//formspree.io/bk@bryankveton.com',
+        method: 'POST',
+        data: $(this).serialize(),
+        dataType: 'json',
+        beforeSend: function() {
+          $contactForm.append('<div class="alert alert--loading">Sending message…</div>');
+        },
+        success: function(data) {
+          $contactForm.find('.alert--loading').hide();
+          $contactForm.append('<div class="alert alert--success">Message sent!</div>');
+        },
+        error: function(err) {
+          $contactForm.find('.alert--loading').hide();
+          $contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
+        }
+      });
+
+    }
+
+  });
+
+  $('input[type="text"], input[type="email"]').focus(function() {
+     if ($(this).hasClass('error')) {
+         $(this).keyup(function() {
+             $(this).removeClass('error');
+         });
+     }
+  });
+
 })();
